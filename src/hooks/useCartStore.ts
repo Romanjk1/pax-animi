149ef2ac9c53024a4908ac4b482e70b84a5568e6
Roman <olomukororoman@gmail.com>
2024-11-05 22:03:27 +0100

    subtotal

--- src/hooks/useCartStore.ts
+++ src/hooks/useCartStore.ts
@@ -14,26 +14,32 @@ type CartState = {
     quantity: number
   ) => void
   removeItem: (wixClient: WixClient, itemId: string) => void
+  computeSubtotal: () => number
 }
 
-export const useCartStore = create<CartState>((set) => ({
-  cart: [],
+export const useCartStore = create<CartState>((set, get) => ({
+  cart: {
+    lineItems: [],
+  },
   isLoading: true,
   counter: 0,
+
   getCart: async (wixClient) => {
     try {
       const cart = await wixClient.currentCart.getCurrentCart()
       set({
-        cart: cart || [],
+        cart: cart || { lineItems: [] },
         isLoading: false,
         counter: cart?.lineItems.length || 0,
       })
     } catch (err) {
-      set((prev) => ({ ...prev, isLoading: false }))
+      set({ isLoading: false })
     }
   },
+
   addItem: async (wixClient, productId, variantId, quantity) => {
-    set((state) => ({ ...state, isLoading: true }))
+    set({ isLoading: true })
+
     const response = await wixClient.currentCart.addToCurrentCart({
       lineItems: [
         {
@@ -49,20 +55,38 @@ export const useCartStore = create<CartState>((set) => ({
 
     set({
       cart: response.cart,
-      counter: response.cart?.lineItems.length,
+      counter: response.cart?.lineItems.length || 0,
       isLoading: false,
     })
   },
+
   removeItem: async (wixClient, itemId) => {
-    set((state) => ({ ...state, isLoading: true }))
+    set({ isLoading: true })
+
     const response = await wixClient.currentCart.removeLineItemsFromCurrentCart(
       [itemId]
     )
 
     set({
       cart: response.cart,
-      counter: response.cart?.lineItems.length,
+      counter: response.cart?.lineItems.length || 0,
       isLoading: false,
     })
+  },
+
+  computeSubtotal: () => {
+    const state = get()
+
+    // Get the line items, default to an empty array if undefined
+    const lineItems = state.cart.lineItems || []
+
+    // Calculate the subtotal by iterating through each line item
+    return lineItems.reduce((total, item) => {
+      // Safely extract the price and convert it to a number
+      const priceAmount = parseFloat(item.price?.amount ?? '0') // Convert to number, default to 0
+
+      // Return the updated total, considering quantity
+      return total + priceAmount * (item.quantity || 1)
+    }, 0)
   },
 }))
